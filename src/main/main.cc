accb7e1a964c7e6a90ccf5585c27e59732d11fd9
Weiqiang Qian <weiqiangqian@WeiqiangdeMacBook-Pro.local>
2022-05-01 23:17:41 -0700

    report modify

--- src/main/main.cc
+++ src/main/main.cc
@@ -14,31 +14,31 @@ int main() {
     
     
     TrojanMap tmap;
-    std::vector<std::string> ids;
-    for( size_t i=0; i<10; ++i ){
-        ids.push_back(tmap.v_Name_node_[ rand()%tmap.v_Name_node_.size() ].second->id );
-    }
-    
-    auto ids_copy = ids;
-    
-    auto v1 = tmap.TravellingTrojan_Brute_force(ids);
-    assert( ids_copy==ids );
-    auto v2 = tmap.TravellingTrojan_Backtracking(ids);
-    auto v3 = tmap.TravellingTrojan_2opt(ids);
-    assert(v1.first == v2.first);
-    assert(v1.first <= v3.first);
+//    std::vector<std::string> ids;
+//    for( size_t i=0; i<10; ++i ){
+//        ids.push_back(tmap.v_Name_node_[ rand()%tmap.v_Name_node_.size() ].second->id );
+//    }
+//
+//    auto ids_copy = ids;
+//
+//    auto v1 = tmap.TravellingTrojan_Brute_force(ids);
+//    assert( ids_copy==ids );
+//    auto v2 = tmap.TravellingTrojan_Backtracking(ids);
+//    auto v3 = tmap.TravellingTrojan_2opt(ids);
+//    assert(v1.first == v2.first);
+//    assert(v1.first <= v3.first);
 
     
     
     
-//    MapUI x;
-//#ifdef NCURSES
-//    x.PlotMap();
-//    x.DynamicPrintMenu();
-//#else
-//    x.PlotMap();
-//    x.PrintMenu();
-//#endif
+    MapUI x;
+#ifdef NCURSES
+    x.PlotMap();
+    x.DynamicPrintMenu();
+#else
+    x.PlotMap();
+    x.PrintMenu();
+#endif
 
     
 }
